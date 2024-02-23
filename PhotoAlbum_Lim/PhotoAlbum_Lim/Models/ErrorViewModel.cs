2205bd549c141edb81c42082d574d7b63024c181
unknown <UU641@ccs.local>
2024-02-24 06:30:19 +0800

    Commit for Photo Album

---
+++ PhotoAlbum_Lim/PhotoAlbum_Lim/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PhotoAlbum_Lim.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
